001f83066a1ecb220c9509fb6e471e85dc2e994b
Paulus Esterhazy <pesterhazy@gmail.com>
2017-06-27 16:19:36 +0200

    Customize reagent version

--- src/foo/main.cljs
+++ src/foo/main.cljs
@@ -1,4 +1,12 @@
 (ns foo.main
   (:require [reagent.core :as r]))
 
+(enable-console-print!)
+
+;; (defonce my-atom (atom nil))
+
+;; (let [a (r/wrap 15 swap! my-atom assoc :foo)]
+;;   (reset! a 20)
+;;   (prn [:a @a]))
+
 (js/console.log (apply (r/partial + 1) [1 1]))
